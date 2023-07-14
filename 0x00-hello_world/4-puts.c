9fc7caef6adee15557da28bd9cb5b1f014f0f1ed
alemayehumoa <alemayehumoa@gmail.com>
2023-07-14 20:20:49 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - using puts
+ * Return: 0
+ */
+#include <stdio.h>
 
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
